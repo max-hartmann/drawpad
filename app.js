09e266c6244190eb0f27bae31f22f6e24fa1035d
Max <maxhendrik@web.de>
2022-10-03 12:06:55 +0200

    Add initial drawing functionality

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const drawingArea = document.querySelector("#drawingArea");
-createGrid(16);
+const drawingContainer = document.querySelector(".drawingContainer");
+createGrid(32);
+let mouseDown = false;
 
-
+drawingContainer.addEventListener("mousedown", () => mouseDown = true);
+drawingContainer.addEventListener("mouseup", () => mouseDown = false);
 
 function createGrid(numberOfTiles) {
+    /* Remove pre existing drawing area */
+    if(document.querySelector("#drawingArea")) document.querySelector("#drawingArea").remove();   
+    
+    const drawingArea = document.createElement("div");
+    drawingArea.setAttribute("id", "drawingArea");
+    drawingContainer.append(drawingArea);
+      
+
     for(let i=0; i<numberOfTiles*numberOfTiles;i++) {
         const newTile = document.createElement("div");
         newTile.classList.add("drawingTile");
+        newTile.addEventListener('mouseenter', drawOnTile);
         drawingArea.append(newTile);
-        console.log("Tile created");
     }
 
     drawingArea.setAttribute("style", `grid-template-columns: repeat(${numberOfTiles}, 1fr);grid-template-rows: repeat(${numberOfTiles}, 1fr);`)
+
+}
+
+function drawOnTile() {
+    if(mouseDown)
+    this.setAttribute("style", "background: black");
 }
